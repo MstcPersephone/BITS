d1d9545bd91edefdffd438b9b0b24c33f6cb04db
Derek Kandler <16686110@mstc.edu>
2020-02-11 18:35:34 -0600

    Added Components for Assessment CRUD operations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AssessmentListComponent } from './assessments/assessment-list/assessment-list.component';
+import { CreateAssessmentComponent } from './assessments/create-assessment/create-assessment.component';
+import { ViewAssessmentComponent } from './assessments/view-assessment/view-assessment.component';
+import { EditAssessmentComponent } from './assessments/edit-assessment/edit-assessment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssessmentListComponent,
+    CreateAssessmentComponent,
+    ViewAssessmentComponent,
+    EditAssessmentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/assessments/view-assessment/view-assessment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-assessment',
+  templateUrl: './view-assessment.component.html',
+  styleUrls: ['./view-assessment.component.css']
+})
+export class ViewAssessmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
