5e95dd8dd313099ceb23ffc532a3d4f6cd428f02
@detertj <detert8076@my.mstc.edu>
2020-02-14 08:52:26 -0600

    Created a material.module.ts for importing all angular material component for styling. Added import of material.module.ts to the app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
+
 import { AssessmentListComponent } from './assessments/assessment-list/assessment-list.component';
 import { CreateAssessmentComponent } from './assessments/create-assessment/create-assessment.component';
 import { ViewAssessmentComponent } from './assessments/view-assessment/view-assessment.component';
@@ -10,7 +14,6 @@ import { EditAssessmentComponent } from './assessments/edit-assessment/edit-asse
 import { AssessmentService } from './services/assessment.service';
 import { AttachmentService } from './services/attachment.service';
 import { QuestionService } from './services/question.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [
     AssessmentService,
