3f600fe450e23af3880f49f7ee05482fbfe968fe
Orion <olson7073@my.mstc.edu>
2020-02-18 13:55:05 -0600

    PER-13 Set up component files for questions and question types. Items can be found in the 'questions' folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,17 @@ import { QuestionService } from './services/question.service';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { ListQuestionsComponent } from './questions/list-questions/list-questions.component';
+import { CreateQuestionComponent } from './questions/create-question/create-question.component';
+import { ViewQuestionComponent } from './questions/view-question/view-question.component';
+import { EditQuestionComponent } from './questions/edit-question/edit-question.component';
+
+import { MultipleChoiceComponent } from './questions/questionTypes/multiple-choice/multiple-choice.component';
+import { TrueFalseComponent } from './questions/questionTypes/true-false/true-false.component';
+import { EssayComponent } from './questions/questionTypes/essay/essay.component';
+import { UploadComponent } from './questions/questionTypes/upload/upload.component';
+import { CheckBoxComponent } from './questions/questionTypes/check-box/check-box.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +35,16 @@ import { AppRoutingModule } from './app-routing.module';
     CreateAssessmentComponent,
     ViewAssessmentComponent,
     EditAssessmentComponent,
-    HeaderComponent
+    HeaderComponent,
+    ListQuestionsComponent,
+    CreateQuestionComponent,
+    ViewQuestionComponent,
+    EditQuestionComponent,
+    MultipleChoiceComponent,
+    TrueFalseComponent,
+    EssayComponent,
+    UploadComponent,
+    CheckBoxComponent
   ],
   imports: [
     BrowserModule,
