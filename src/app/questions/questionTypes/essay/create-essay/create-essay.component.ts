37cf3013e4a2a25ab25a1023e7051c40268ccdfd
Tegan Kennedy <kennedy2068@mstc.edu>
2020-02-20 15:30:32 -0600

    PER-36 setting up bindings for essay question creation

--- src/app/questions/questionTypes/essay/create-essay/create-essay.component.ts
+++ src/app/questions/questionTypes/essay/create-essay/create-essay.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Essay } from 'src/app/models/question-types/essay.model';
 
 @Component({
   selector: 'app-create-essay',
@@ -6,11 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-essay.component.css']
 })
 export class CreateEssayComponent implements OnInit {
+// The form object
+  createEssayForm;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
+    this.createEssayForm = this.formBuilder.group({
+      essayName: '',
+      essayText: '',
+      essayAnswer: ''
+    });
      }
 
   ngOnInit(): void {
   }
 
+    // Id is null at this point because it is generated on the backend.
+    onSubmit(questionData) {
+      const question =  new Essay({
+        id: null,
+        name: questionData.essayName,
+        questionText: questionData.essayText,
+        hasAttachments: false,
+        attachments: null,
+        isAnswered: false,
+        answer: questionData.essayAnswer,
+        duration: 0
+      });
+
+      // Resets the form values.
+      this.createEssayForm.reset();
+
+      // For testing, we can remove later.
+      console.log(question);
+    }
 }
