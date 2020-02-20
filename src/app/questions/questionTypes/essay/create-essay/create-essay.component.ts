421f1c85d1823f0e913dd48e1a49e15e1fc3ac0f
Tegan Kennedy <kennedy2068@mstc.edu>
2020-02-20 15:02:41 -0600

    PER-36 html for create essay question

--- src/app/questions/questionTypes/essay/create-essay/create-essay.component.ts
+++ src/app/questions/questionTypes/essay/create-essay/create-essay.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEssayComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+     }
 
   ngOnInit(): void {
   }
