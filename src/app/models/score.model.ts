1e7dfe8ddb216b1cbacd921dc8cbf4f3bee5934b
Derek Kandler <16686110@mstc.edu>
2020-02-23 12:44:28 -0600

    Added id properties to student model

--- src/app/models/score.model.ts
+++ src/app/models/score.model.ts
@@ -1,5 +1,7 @@
 // Represents a score from taking an assessment
 export class Score {
+  id: string;
+  assessmentId: string;
   numberOfQuestionsAsked: number;
   numberOfCorrectAnswers: number;
   isPassingScore: boolean;
