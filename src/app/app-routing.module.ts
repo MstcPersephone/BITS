92e3acba158cbe30279153839900efba261f9b0c
@detertj <detert8076@my.mstc.edu>
2020-02-24 10:03:13 -0600

    PER-49 NoDisplayForTrueFalse

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { CreateOptionComponent } from './shared/option/create-option/create-opti
 import { CreateExactMatchComponent } from './exact-match/create-exact-match/create-exact-match.component';
 
 import { ListCheckboxComponent } from './questions/questionTypes/check-box/list-checkbox/list-checkbox.component';
+import { ListTrueFalseComponent } from './questions/questionTypes/true-false/list-true-false/list-true-false.component';
 
 // Path naming convention: item/action ie: assessment/create
 const routes: Routes = [
@@ -26,6 +27,7 @@ const routes: Routes = [
   { path: 'match/create', component: CreateExactMatchComponent },
   { path: 'question/list', component: ListQuestionsComponent },
   { path: 'question/checkbox/list', component: ListCheckboxComponent },
+  { path: 'question/true-false/list', component: ListTrueFalseComponent },
   { path: 'question/view', component: ViewQuestionComponent }
 ];
 
