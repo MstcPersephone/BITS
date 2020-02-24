e745e5d40b06a2f291bdb77feee0c1cf6a145562
Derek Kandler <16686110@mstc.edu>
2020-02-23 19:54:36 -0600

    Made changes to allow saving of multiple choice and short answer. Created backend model for ExactMatch Mapped new object to ShortAnswer schema Refactored Essay schema to be ShortAnswer Added casses and DTOs for ShortAnswer and MultipleChoice Added CreateMultipleChoice template and logic

--- src/app/questions/questionTypes/short-answer/create-short-answer/create-short-answer.component.ts
+++ src/app/questions/questionTypes/short-answer/create-short-answer/create-short-answer.component.ts
@@ -12,12 +12,11 @@ import { AttachmentService } from 'src/app/services/attachment.service';
 export class CreateShortAnswerComponent implements OnInit {
 // The form object
   createShortAnswerForm;
-  hasAttachments = false;
 
   constructor(
     private formBuilder: FormBuilder,
     private questionService: QuestionService,
-    private attachmentSerivce: AttachmentService
+    public attachmentService: AttachmentService
               ) {
     this.createShortAnswerForm = this.formBuilder.group({
       questionText: '',
@@ -34,8 +33,8 @@ export class CreateShortAnswerComponent implements OnInit {
       const shortAnswerQuestion: ShortAnswer =  new ShortAnswer();
       shortAnswerQuestion.id = null;
       shortAnswerQuestion.questionText = questionData.questionText;
-      shortAnswerQuestion.hasAttachments = this.hasAttachments;
-      shortAnswerQuestion.attachments = this.hasAttachments ? this.attachmentSerivce.getAttachments() : null;
+      shortAnswerQuestion.hasAttachments = this.attachmentService.hasAttachments;
+      shortAnswerQuestion.attachments = this.attachmentService.hasAttachments ? this.attachmentService.getAttachments() : null;
       shortAnswerQuestion.isAnswered = false;
       shortAnswerQuestion.matches = this.questionService.getMatches();
       shortAnswerQuestion.duration = 0;
@@ -50,9 +49,5 @@ export class CreateShortAnswerComponent implements OnInit {
 
       // For testing, we can remove later.
       console.log(shortAnswerQuestion);
-    }
-
-    hasAttachmentsChanged() {
-      this.hasAttachments = !this.hasAttachments;
     }
 }
