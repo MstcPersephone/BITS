fbdf0bf73675d9bf497912334001db167168f89b
Tegan Kennedy <kennedy2068@mstc.edu>
2020-03-03 16:25:32 -0600

    PER-61 initial commit setting up viewing for multiple choice questions

--- src/app/questions/questionTypes/multiple-choice/view-multiple-choice/view-multiple-choice.component.ts
+++ src/app/questions/questionTypes/multiple-choice/view-multiple-choice/view-multiple-choice.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MultipleChoice } from 'src/app/models/question-types/multiple-choice.model';
+import { FormBuilder } from '@angular/forms';
+import { AssessmentService } from 'src/app/services/assessment.service';
 
 @Component({
   selector: 'app-view-multiple-choice',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-multiple-choice.component.css']
 })
 export class ViewMultipleChoiceComponent implements OnInit {
+  answerMultipleChoiceForm;
+  @Input() question: MultipleChoice;
 
-  constructor() { }
+  constructor(
+    private assessmentService: AssessmentService,
+    private formBuilder: FormBuilder
+  ) {
+    // Although there are no fields,
+    // We still want to create the form and use angular forms
+    // to handle the submit function
+    this.answerMultipleChoiceForm = this.formBuilder.group({});
+   }
 
   ngOnInit(): void {
   }
 
+  onChangeOption($event, i) {
+    this.question.options[i].optionIsSelected = $event.checked;
+    console.log(this.question.options);
+  }
+
+  onSubmit() {
+    this.assessmentService.submitAnswer(this.question);
+  }
 }
