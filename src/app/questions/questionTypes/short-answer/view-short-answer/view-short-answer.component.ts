b8505e99a4df4ab3d95fe2c25132d8c6d85a70d6
Tegan Kennedy <kennedy2068@mstc.edu>
2020-03-04 14:51:37 -0600

    PER-62 initial setup for viewing short answer questions

--- src/app/questions/questionTypes/short-answer/view-short-answer/view-short-answer.component.ts
+++ src/app/questions/questionTypes/short-answer/view-short-answer/view-short-answer.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ShortAnswer } from 'src/app/models/question-types/short-answer.model';
+import { FormBuilder } from '@angular/forms';
+import { AssessmentService } from 'src/app/services/assessment.service';
 
 @Component({
   selector: 'app-view-short-answer',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-short-answer.component.css']
 })
 export class ViewShortAnswerComponent implements OnInit {
+answerShortAnswerForm;
+@Input() question: ShortAnswer;
 
-  constructor() { }
+  constructor(
+    private assessmentService: AssessmentService,
+    private formBuilder: FormBuilder
+  ) {
+    // Although there are no fields,
+    // We still want to create the form and use angular forms
+    // to handle the submit function
+    this.answerShortAnswerForm = this.formBuilder.group({});
+   }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.assessmentService.submitAnswer(this.question);
+  }
 }
