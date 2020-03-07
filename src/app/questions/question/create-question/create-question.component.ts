7fbacce7a1a38bbb63477354a6457f2e47277895
@detertj <detert8076@my.mstc.edu>
2020-03-07 16:23:51 -0600

    PER-109 Added points to the template. Added logic to store and pass the value to the database.

--- src/app/questions/question/create-question/create-question.component.ts
+++ src/app/questions/question/create-question/create-question.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HelperService } from '../../../services/helper.service';
+import { FormBuilder } from '@angular/forms';
+import { QuestionService } from 'src/app/services/question.service';
 
 @Component({
   selector: 'app-create-question',
@@ -8,11 +10,19 @@ import { HelperService } from '../../../services/helper.service';
 })
 export class CreateQuestionComponent implements OnInit {
   questionSelectionFormGroup;
+  addPointForm;
 
   questionTypes = [];
   questionTypeSelected: string;
 
-  constructor(private helperService: HelperService) { }
+  constructor(public questionService: QuestionService,
+              private formBuilder: FormBuilder,
+              private helperService: HelperService) {
+    // Creates an object to hold form values.
+    this.addPointForm = this.formBuilder.group({
+      points: ''
+    });
+  }
 
   ngOnInit(): void {
     // gets the question types from the QuestionType enum
