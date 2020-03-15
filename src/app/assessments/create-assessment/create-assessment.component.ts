c12c124241c10e1ecfe03a79162353dda105104e
@detertj <detert8076@my.mstc.edu>
2020-03-15 12:24:59 -0500

    -Updated the template with a category selection form. -Added question loading by category selection. -Updated the template to display the properties available question list. -Updated the template to display the properties of the assessment question list.

--- src/app/assessments/create-assessment/create-assessment.component.ts
+++ src/app/assessments/create-assessment/create-assessment.component.ts
@@ -6,7 +6,8 @@ import { Subscription } from 'rxjs';
 import { Category } from 'src/app/models/shared/category.model';
 import { Question } from 'src/app/models/question.interface';
 import { Assessment } from 'src/app/models/assessment.model';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { FormBuilder, FormArray } from '@angular/forms';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-create-assessment',
@@ -14,31 +15,65 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./create-assessment.component.css']
 })
 export class CreateAssessmentComponent implements OnInit {
+  public categories: Category[] = [];
+  private categorySubscription: Subscription;
   public questions: Question[] = [];
+  public organizedQuestions = {};
   private questionSubscription: Subscription;
   public assessmentQuestions = [] = [];
+  selectCategoryForm;
+  public selectedCategory: Category;
+  public selectedName: any;
 
   constructor(
     public questionService: QuestionService,
     public attachmentService: AttachmentService,
-    public helperService: HelperService
-  ) { }
+    public helperService: HelperService,
+    private formBuilder: FormBuilder) {
+    this.selectCategoryForm = this.formBuilder.group({
+      categories: ''
+    });
+  }
 
   ngOnInit() {
+
+    this.getCategoriesSelection();
+
+    this.getOrganizedQuestions();
+  }
+
+  getCategoriesSelection() {
+    // gets the call to api end point to collect all categories from database
+    this.questionService.getAllCategories();
+    this.questionSubscription = this.questionService.getCategoriesListener()
+      .subscribe((categoriesArray: Category[]) => {
+        this.categories = categoriesArray;
+        // call to function to short questions by category
+        console.log('All Categories: ');
+        console.log(this.categories);
+      });
+  }
+
+  setAssessmentAvailableQuestions(event: any, selectObject: any, selectCategoryForm: any) {
+
+    this.selectedCategory = event.value;
+    this.selectedName = this.selectedCategory.name;
+    console.log('Selected Category: ');
+    console.log(this.selectedCategory.name);
+  }
+
+  getOrganizedQuestions() {
     // gets the call to api end point to collect all questions from database
     this.questionService.getAllQuestions();
     // subsribe to observer to get question array changes
     this.questionSubscription = this.questionService.getQuestionsUpdatedListener()
-      .subscribe((questionsArray: Question[]) => {
-        this.questions = questionsArray;
+      .subscribe((questionsArray: any) => {
+        this.organizedQuestions = questionsArray;
         // call to function to short questions by category
         console.log('All Questions: ');
-        console.log(this.questions);
+        console.log(this.organizedQuestions);
+
       });
-  }
-
-  buildAssessmentQuestions() {
-
   }
 
   drop(event: CdkDragDrop<string[]>) {
@@ -46,13 +81,15 @@ export class CreateAssessmentComponent implements OnInit {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
     }
-    console.log('Available Questions: ');
+    console.log('Question moved FROM: ');
+    console.log(event.previousContainer.element);
     console.log(event.previousContainer.data);
-    console.log('Assessment Questions: ');
+    console.log('Question moved TO: ');
+    console.log(event.container.element);
     console.log(event.container.data);
   }
 
