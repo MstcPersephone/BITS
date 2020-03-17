9291252d046071bbcb2523f0aba6ee440259e303
Tegan Kennedy <kennedy2068@mstc.edu>
2020-03-17 12:56:07 -0500

    Fleshed out html and ts for edit true false

--- src/app/questions/questionTypes/true-false/edit-true-false/edit-true-false.component.ts
+++ src/app/questions/questionTypes/true-false/edit-true-false/edit-true-false.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Question } from 'src/app/models/question.interface';
 import { FormBuilder } from '@angular/forms';
 import { QuestionService } from 'src/app/services/question.service';
+import { HelperService } from 'src/app/services/helper.service';
 import { AttachmentService } from 'src/app/services/attachment.service';
 import { TrueFalse } from 'src/app/models/question-types/true-false.model';
 
@@ -15,10 +16,16 @@ export class EditTrueFalseComponent implements OnInit {
   @Input() question: Question;
 // The true false edit form used to update the quesiton
   editTrueFalseForm;
+  selectedAnswer = '';
+  answers: any = [
+    'True',
+    'False'
+  ];
   constructor(
     public questionService: QuestionService,
     private formBuilder: FormBuilder,
-    public attachmentService: AttachmentService
+    public attachmentService: AttachmentService,
+    private helperService: HelperService
   ) {
     this.editTrueFalseForm = this.formBuilder.group({
       questionText: '',
@@ -38,4 +45,24 @@ export class EditTrueFalseComponent implements OnInit {
     }
   }
 
+  radioChangeHandler(event: any) {
+    this.selectedAnswer = event.value;
+  }
+
+  onSubmit(formData) {
+    const updatedTrueFalseQuestion: TrueFalse = new TrueFalse();
+    console.log(formData);
+    updatedTrueFalseQuestion._id = this.question._id;
+    updatedTrueFalseQuestion.questionText = formData.questionText === '' ? this.question.questionText : formData.questionText;
+    updatedTrueFalseQuestion.hasAttachments = this.attachmentService.hasAttachments;
+    updatedTrueFalseQuestion.attachments = this.attachmentService.hasAttachments ? this.attachmentService.getAttachments() : [];
+    updatedTrueFalseQuestion.answer = this.helperService.convertToTrueFalse(formData.answer);
+    updatedTrueFalseQuestion.isAnswered = false;
+    updatedTrueFalseQuestion.studentAnswer = null;
+    updatedTrueFalseQuestion.duration = 0;
+    updatedTrueFalseQuestion.assessmentIds = null;
+
+    this.questionService.updateQuestionById(updatedTrueFalseQuestion);
+    console.log(formData);
+  }
 }
