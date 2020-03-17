74b3f0ae408b941a42fcdd13d4b1137d83c92055
Tegan Kennedy <kennedy2068@mstc.edu>
2020-03-17 11:37:17 -0500

    Edit true false initial commit

--- src/app/questions/questionTypes/true-false/edit-true-false/edit-true-false.component.ts
+++ src/app/questions/questionTypes/true-false/edit-true-false/edit-true-false.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Question } from 'src/app/models/question.interface';
+import { FormBuilder } from '@angular/forms';
+import { QuestionService } from 'src/app/services/question.service';
+import { AttachmentService } from 'src/app/services/attachment.service';
+import { TrueFalse } from 'src/app/models/question-types/true-false.model';
 
 @Component({
   selector: 'app-edit-true-false',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-true-false.component.css']
 })
 export class EditTrueFalseComponent implements OnInit {
-
-  constructor() { }
+// The question being edited
+  @Input() question: Question;
+// The true false edit form used to update the quesiton
+  editTrueFalseForm;
+  constructor(
+    public questionService: QuestionService,
+    private formBuilder: FormBuilder,
+    public attachmentService: AttachmentService
+  ) {
+    this.editTrueFalseForm = this.formBuilder.group({
+      questionText: '',
+      hasAttachments: ''
+    });
+  }
 
   ngOnInit(): void {
+    console.log(this.question);
+    // Pass the attachments off to the attachment service to be managed.
+    if (this.question.hasAttachments) {
+      this.attachmentService.attachments = this.question.attachments;
+      this.attachmentService.hasAttachments = true;
+      this.attachmentService.hasAttachmentFileNames = true;
+    } else {
+      this.attachmentService.attachments = [];
+    }
   }
 
 }
