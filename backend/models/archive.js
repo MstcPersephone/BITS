fb7acd1dd49923de638e34be71ada6ab1fa50b28
Tegan Kennedy <kennedy2068@mstc.edu>
2020-03-25 12:48:57 -0500

    added schema to archive model

--- backend/models/archive.js
+++ backend/models/archive.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 // Empty Question Schema
 // Used to access the question collection generically
-const questionSchema = mongoose.Schema({});
+const archiveSchema = mongoose.Schema({});
 
 // Create and export Question Shell Model
-module.exports = mongoose.model('Archive', questionSchema, 'archive');
+module.exports = mongoose.model('Archive', archiveSchema, 'archive');
