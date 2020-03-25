b92723f36c1019d6f9e122099b3ff16761d470a0
@detertj <detert8076@my.mstc.edu>
2020-03-25 18:23:04 -0500

    - Have all questions showing except for question type value

--- src/app/assessments/assessment/view-assessment/view-assessment.component.ts
+++ src/app/assessments/assessment/view-assessment/view-assessment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { Assessment } from 'src/app/models/assessment.model';
@@ -6,22 +6,31 @@ import { AssessmentConfig } from 'src/app/models/assessment-config.model';
 import { AssessmentService } from 'src/app/services/assessment.service';
 import { Question } from 'src/app/models/question.interface';
 import { QuestionService } from 'src/app/services/question.service';
+import { Router } from '@angular/router';
+import { MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-view-assessment',
   templateUrl: './view-assessment.component.html',
   styleUrls: ['./view-assessment.component.css']
 })
-export class ViewAssessmentComponent implements OnInit {
+export class ViewAssessmentComponent implements OnInit, AfterViewInit {
   questions: Question[];
+  question: Question;
   assessment: Assessment;
+  id = this.route.snapshot.params.assessmentId;
   private questionsSubscription: Subscription;
   private assessmentSubscription: Subscription;
+  public displayedColumns: string[] = ['questionType', 'questionText', 'points'];
+  public dataSource = new MatTableDataSource<Question>();
+  public columnsToDisplay = ['questionType', 'questionText', 'points'];
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(
     public questionService: QuestionService,
     public assessmentService: AssessmentService,
-    private route: ActivatedRoute
+    public route: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -31,15 +40,38 @@ export class ViewAssessmentComponent implements OnInit {
         this.assessment = assessment;
         console.log('Assessment by Id', this.assessment);
 
-      //   this.assessmentService.getQuestionsByIds(this.route.snapshot.params.questionIds);
-      //   this.questionsSubscription = this.assessmentService.getAssessmentQuestionsUpdatedListener()
-      // .subscribe((questions: Question[]) => {
-      //   this.questions = questions;
-      //   console.log('Questions by Id', this.questions);
-      // });
+        this.assessmentService.getQuestionsByIds(this.assessmentService.questionIds);
+        this.questionsSubscription = this.assessmentService.getAssessmentQuestionsUpdatedListener()
+          .subscribe((questionsArray: any) => {
+            this.dataSource.data = questionsArray;
+            console.table(this.dataSource.data);
 
+            // this.dataSource.data.forEach((data) => {
+            //   switch (data.questionType) {
+            //     case 'Checkbox': return 'Checkbox';
+            //     case 'Multiple Choice': return 'Multiple Choice';
+            //     case 'Short Answer': return 'Short Answer';
+            //     case 'True False': return 'True False';
+            //     case 'Upload': return 'Upload';
+
+            //     default: return this.dataSource.data;
+            //     }
+            // });
+
+            this.dataSource.sort = this.sort;
+          });
       });
-
-
   }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  reloadOriginalOrder(id) {
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate(['assessment/view/:' + id]);
+    });
+  }
+
+
 }
