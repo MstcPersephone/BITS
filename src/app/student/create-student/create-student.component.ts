5380c188ebc4c7066781d7f8bc0b5d62160ce263
@detertj <detert8076@my.mstc.edu>
2020-03-26 11:22:02 -0500

    - Finished up the template for student

--- src/app/student/create-student/create-student.component.ts
+++ src/app/student/create-student/create-student.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray } from '@angular/forms';
+import { Student } from '../../models/student.model';
+import { AssessmentEngineService } from '../../services/assessment-engine.service';
+import { HelperService } from '../../services/helper.service';
 
 @Component({
   selector: 'app-create-student',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-student.component.css']
 })
 export class CreateStudentComponent implements OnInit {
+  createStudentForm;
+  maxDate: Date;
+  minDate: Date;
+  startDate: Date;
 
-  constructor() { }
+  constructor(
+    public assessmentEngineService: AssessmentEngineService,
+    public helperService: HelperService,
+    private formBuilder: FormBuilder) {
+    this.createStudentForm = this.formBuilder.group({
+      studentId: '',
+      firstName: '',
+      lastName: '',
+      dateOfBirth: '',
+      campusLocation: ''
+    });
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 100, 0, 0);
+    this.maxDate = new Date(currentYear - 16, 0, 0);
+    this.startDate = new Date(currentYear - 21, 1, 1);
+  }
 
   ngOnInit() {
+
+  }
+
+  onSubmit() {
+
   }
 
 }
