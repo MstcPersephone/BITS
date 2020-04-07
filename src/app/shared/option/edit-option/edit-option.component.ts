031141b852126a9baf6b051ef9d71119bcf53eb0
@detertj <detert8076@my.mstc.edu>
2020-04-07 18:25:32 -0500

    edit mulitple choice working with the exception of min option/answer validation

--- src/app/shared/option/edit-option/edit-option.component.ts
+++ src/app/shared/option/edit-option/edit-option.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Option } from 'src/app/models/shared/option.model';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators, FormArray } from '@angular/forms';
 import { QuestionService } from 'src/app/services/question.service';
+import { ValidationService } from 'src/app/services/validation.service';
+import { Option } from 'src/app/models/shared/option.model';
 
 @Component({
   selector: 'app-edit-option',
@@ -16,23 +17,44 @@ export class EditOptionComponent implements OnInit {
     private questionService: QuestionService
   ) {
     this.editOptionForm = this.formBuilder.group({
-      optionText: '',
+      optionText: ['', [Validators.required, ValidationService.invalidWhiteSpaceOnly]],
       isAnswer: ''
     });
-   }
-
-  ngOnInit() {
-    this.editOptionForm.get('isAnswer').setValue(this.option.isAnswer);
   }
 
+  ngOnInit() {
+    this.editOptionForm.setValue({ optionText: String(this.option.optionText) });
+    this.editOptionForm.setValue({ isAnswer: String(this.option.isAnswer) });
+  }
+
+  // Id is null at this point because it is generated on the backend.
   onSubmit(formData) {
+    // This sets validation of the current option in the question service
+    if (!this.editOptionForm.valid) {
+      this.questionService.setOptionInvalid();
+      (Object as any).values(this.editOptionForm.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    } else {
+      this.questionService.setOptionIsValid();
+    }
+
+    // Initializes the option to be updated
     const updatedOption = new Option();
-    updatedOption._id = this.option._id;
-    updatedOption.questionId = this.option.questionId;
-    updatedOption.optionText = formData.optionText;
-    updatedOption.isAnswer = formData.isAnswer;
-    updatedOption.optionIsSelected = false;
-    this.questionService.updateOption(this.option, updatedOption);
-    console.log(updatedOption);
+
+    // If the current option to edit is valid
+    if (this.questionService.optionIsValid) {
+      updatedOption._id = this.option._id;
+      updatedOption.questionId = this.option.questionId;
+      updatedOption.optionText = formData.optionText;
+      updatedOption.isAnswer = formData.isAnswer;
+      updatedOption.optionIsSelected = false;
+
+      // Adds current option to the options array in the service.
+      this.questionService.updateOption(this.option, updatedOption);
+
+      // For testing, we can remove later.
+      console.log(updatedOption);
+    }
   }
 }
