884900f07ef1ddcaad05245ce4edb0d854b05b77
Tegan Kennedy <kennedy2068@mstc.edu>
2020-05-08 14:56:14 -0500

    Why won't it import?!?!?!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import {ClipboardModule} from '@angular/cdk/clipboard';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 import { AssessmentListComponent } from './assessments/assessment/assessment-list/assessment-list.component';
 import { CreateAssessmentComponent } from './assessments/assessment/create-assessment/create-assessment.component';
