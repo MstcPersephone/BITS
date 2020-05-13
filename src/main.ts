d903ffa9d2868a11067c73c4d7c552706300b008
Tegan Kennedy <kennedy2068@mstc.edu>
2020-05-13 10:45:19 -0500

    PER-206 added console.table to hidden logs

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
   if (window) {
-    window.console.log = window.console.warn = window.console.info = function hideLogs() {
+    window.console.log = window.console.warn = window.console.info = window.console.table = function hideLogs() {
       // Don't log anything.
     };
   }
